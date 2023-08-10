c17d7902dbd88d43cb6126acdd52044a365ac7e7
Shivakant6387 <shivakantsingh900@gmail.com>
2023-08-10 08:57:18 +0530

    back end ui connection

---
+++ app/src/main/java/com/example/notification/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.notification
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "AAAA14_oJ1s:APA91bHA9xV74Ks6PgijY7k_2M3_pc_9reKST_yT2GBeKZuLCzx4XOrsUDibZlIVzILqG1JgYVTlF0cw3oes5JVMhnI6Nrafsg34mGow0IujR78EAySFoSmjTHzv3uQty2aK6EJy9Hlj"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
